e705a7c448343fc4bcec53cd2ac0d8e218132825
JohnMcMillan <jmcmillan1873@yahoo.com>
2020-07-18 00:56:05 +0100

    Changes in this commit    - Added encyption policy for first time setup script    - Added Dynamodb creation to FirstTimeSetup.sh plus other refinements

--- FirstTimeSetup/FirstTimeSetup.sh
+++ FirstTimeSetup/FirstTimeSetup.sh
@@ -1,24 +1,24 @@
 #!/bin/sh
 
+# Run a couple of commands that should be first time setup only. 
+# This is my approach to dealing with the chicken/Egg situation between terraform being aware of/managing resources to hold statefile and locking details. 
+
 # Create an S3 bucket to hold the terraform state file
 # This bucket has
 #  - encryption at rest enabled by default
 #  - versioning enabled, to preserve previous versions of tfstate
 #  - lifecycle policy to archive old state file to glacier after 30 days, then delete after 90 days
-#  - default permissions - meaning private, not public. Accessible to local IAM users with S3 permissions. 
+#  - restrictive but functional permissions - meaning private, not public. Accessible to local IAM users with S3 permissions.
 
-aws s3 mb s3://JohnMcMillanECSTechTest
-aws s3api put-bucket-encryption --bucket JohnMcMillanECSTechTest --server-side-encryption-configuration '{  "Rules": [
-    {
-      "ApplyServerSideEncryptionByDefault": {
-        "SSEAlgorithm": "AES256"
-      }
-    }
-  ]
-}'
+BUCKETNAME=johnmcmillanecstechtest
+aws s3 mb s3://$BUCKETNAME
+aws s3api put-bucket-encryption --bucket $BUCKETNAME --server-side-encryption-configuration file://encryption.json
+aws s3api put-bucket-versioning --bucket $BUCKETNAME --versioning-configuration Status=Enabled
+aws s3api put-bucket-lifecycle-configuration --bucket $BUCKETNAME --lifecycle-configuration file://lifecycle.json
+aws s3api put-bucket-acl --acl private --bucket $BUCKETNAME
+aws s3api put-public-access-block --bucket $BUCKETNAME --public-access-block-configuration "BlockPublicAcls=true,IgnorePublicAcls=true,BlockPublicPolicy=true,RestrictPublicBuckets=true"
 
 
-aws s3api put-bucket-versioning --bucket JohnMcMillanECSTechTest --versioning-configuration Status=Enabled
-aws s3api put-bucket-lifecycle-configuration --bucket JohnMcMillanECSTechTest --lifecycle-configuration file://lifecycle.json
 
-
+# Create the DynamoDB table to hold a lock on the statefile to prevent corruption
+aws dynamodb create-table --region eu-west-1 --table-name tf-state-locking --attribute-definitions AttributeName=LockID,AttributeType=S --key-schema AttributeName=LockID,KeyType=HASH --provisioned-throughput ReadCapacityUnits=5,WriteCapacityUnits=5 
