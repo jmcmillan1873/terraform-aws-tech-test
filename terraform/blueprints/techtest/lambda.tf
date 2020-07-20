8f49567dbeee352515e5dd5431ebb1e309d1403b
JohnMcMillan <jmcmillan1873@yahoo.com>
2020-07-20 22:56:15 +0100

    Changes in this update   - added lambda trigger   - assocaited lambda trigger with function

--- terraform/blueprints/techtest/lambda.tf
+++ terraform/blueprints/techtest/lambda.tf
@@ -9,5 +9,25 @@ resource "aws_lambda_function" "ec2reports_lambda" {
    role          = "arn:aws:iam::680558138144:role/service-role/ec2report-to-lambda"
    handler       = "ec2reports-function.lambda_handler"
    timeout       = "20"
-   runtime = "python3.7"
+   runtime       = "python3.7"
+}
+
+
+resource "aws_cloudwatch_event_rule" "ec2reports-schedule" {
+   name                = "ec2reports-schedule"
+   description         = "Runs the ec2reports lambda function every hour"
+   schedule_expression = "cron(37 * * * ? *)"
+}
+
+resource "aws_cloudwatch_event_target" "ec2reports-target" {
+   rule = aws_cloudwatch_event_rule.ec2reports-schedule.name
+   arn  = aws_lambda_function.ec2reports_lambda.arn
+}
+
+resource "aws_lambda_permission" "ec2reports-caller" {
+   statement_id  = "AllowExecutionFromCloudWatch"
+   action        = "lambda:InvokeFunction"
+   function_name = aws_lambda_function.ec2reports_lambda.function_name
+   principal     = "events.amazonaws.com"
+   source_arn    = aws_cloudwatch_event_rule.ec2reports-schedule.arn
 }
